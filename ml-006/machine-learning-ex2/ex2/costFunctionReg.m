a2dcdbf8a28f2276a87ddf6d666492307fccfd9c
rusticsankalp <rusticsankalp@outlook.com>
2016-01-03 04:36:36 -0800

    Incomplete second assm

--- ml-006/machine-learning-ex2/ex2/costFunctionReg.m
+++ ml-006/machine-learning-ex2/ex2/costFunctionReg.m
@@ -11,11 +11,30 @@ m = length(y); % number of training examples
 J = 0;
 grad = zeros(size(theta));
 
+
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
+htheta = sigmoid(X * theta);
+j1 = -y.*log(htheta);
+j2 = -(1-y).*log(1-htheta);
+jall = j1 + j2;
+
+thetasq = (theta.*theta);
+regsum  = sum(thetasq) - thetasq(1);
+regcost = lambda*regsum/(2 * m);
+
+J = sum (jall)/m+regcost;
+
+resdiff =  (htheta - y );
+resdiff2 = resdiff.*X;
+ressum = sum(resdiff2) /m ;
+grad = ressum';
+
+
 
 
 
