85e217a806cb055304d9ce26a53d21669ed6e24e
Sankalp Gupta <sankalpg@microsoft.com>
2016-02-11 16:14:57 -0800

    Percolation empty

---
+++ algs4part1_feb_2016/01ProgAssm_Percolation/code/Percolation.java
@@ -0,0 +1,28 @@
+public class Percolation {
+   public Percolation(int N)               // create N-by-N grid, with all sites blocked
+   {
+     
+   }
+   public void open(int i, int j)          // open site (row i, column j) if it is not open already
+   {
+   }
+   public boolean isOpen(int i, int j)     // is site (row i, column j) open?
+   {
+     return false;
+   }
+   public boolean isFull(int i, int j)     // is site (row i, column j) full?
+     {
+     return false;
+   }
+   public boolean percolates()             // does the system percolate?
+     {
+     
+     return false;
+   }
+
+   public static void main(String[] args)  // test client (optional)
+     {
+
+   }
+}
+
